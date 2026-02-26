41e247d7da1917717f701686587fb8345e38654e
RampsosPeck <peralta.jorge.uatf@gmail.com>
2026-02-26 11:56:24 -0400

    first commit

---
+++ notes.sql
@@ -0,0 +1,22 @@
+--********************************************
+--URl curs https://www.youtube.com/watch?v=3GymExBkKjE
+--********************************************
+
+
+--1) Primero cambiar las variables de git config y name
+
+--2) Pusimos nombre a la rama master
+
+# git branch -m development
+
+--3) Para ver el estado de nuestro proyecto
+
+# git status
+
+--4) Añadimos el fichero para subirlo 
+
+# git add  notes.sql
+
+--5)
+
+--6)
